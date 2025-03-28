d99219db8423e749d1c5987fdc1504fe81417c25
Casey Barbello <caseyjbarbello@gmail.com>
2025-03-28 02:28:09 -0700

    add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
-
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Button } from "@/components/ui/button";
 import { ProjectCard } from "@/components/project-card";
 import { BlogPostCard } from "@/components/blog-post-card";
@@ -20,104 +20,107 @@ const featuredBlogPosts = blogPosts.filter((blogPost) =>
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
+      <GoogleAnalytics gaId="G-KSCWGZC3DE" />
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-        <div className="container px-4 md:px-6">
-          <div className="flex flex-col items-center space-y-4 text-center">
-            <div className="space-y-2">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                Casey Barbello
-              </h1>
+      <div className="flex flex-col min-h-screen">
+        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+          <div className="container px-4 md:px-6">
+            <div className="flex flex-col items-center space-y-4 text-center">
+              <div className="space-y-2">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                  Casey Barbello
+                </h1>
+                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                  Senior Full Stack Software Engineer
+                </p>
+              </div>
               <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                Senior Full Stack Software Engineer
+                {"(This site is under construction)"}
               </p>
+              <div className="space-x-4">
+                <Button asChild>
+                  <Link
+                    href="https://github.com/cbarbello/portfolio"
+                    target="_blank"
+                  >
+                    Source Code
+                  </Link>
+                </Button>
+                <Button variant="outline" asChild>
+                  <Link href="/contact">Contact Me</Link>
+                </Button>
+              </div>
             </div>
-            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-              {"(This site is under construction)"}
-            </p>
-            <div className="space-x-4">
-              <Button asChild>
-                <Link
-                  href="https://github.com/cbarbello/portfolio"
-                  target="_blank"
-                >
-                  Source Code
+          </div>
+        </section>
+
+        {/* Featured Projects */}
+        <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-50 dark:bg-gray-900">
+          <div className="container px-4 md:px-6">
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Featured Projects
+                </h2>
+              </div>
+            </div>
+            <div className="mx-auto grid max-w-5xl grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 mt-8">
+              {featuredProjects.map((project) => (
+                <ProjectCard
+                  key={project.id}
+                  title={project.title}
+                  description={project.description}
+                  image={project.image}
+                  tags={project.tags}
+                  link={project.link}
+                />
+              ))}
+            </div>
+            <div className="flex justify-center mt-8">
+              <Button variant="outline" asChild>
+                <Link href="/projects" className="flex items-center">
+                  View All Projects
+                  <ArrowRight className="ml-2 h-4 w-4" />
                 </Link>
               </Button>
+            </div>
+          </div>
+        </section>
+
+        {/* Featured Blog Posts */}
+        <section className="w-full py-12 md:py-24 lg:py-32">
+          <div className="container px-4 md:px-6">
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Featured Blog Posts
+                </h2>
+              </div>
+              {featuredBlogPosts.map((blogPost) => (
+                <BlogPostCard
+                  key={blogPost.id}
+                  title={blogPost.title}
+                  excerpt={blogPost.excerpt}
+                  date={blogPost.date}
+                  image={blogPost.image}
+                  slug={blogPost.slug}
+                />
+              ))}
+            </div>
+            <div className="flex justify-center mt-8">
               <Button variant="outline" asChild>
-                <Link href="/contact">Contact Me</Link>
+                <Link href="/blog" className="flex items-center">
+                  Read All Posts
+                  <ArrowRight className="ml-2 h-4 w-4" />
+                </Link>
               </Button>
             </div>
           </div>
-        </div>
-      </section>
-
-      {/* Featured Projects */}
-      <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-50 dark:bg-gray-900">
-        <div className="container px-4 md:px-6">
-          <div className="flex flex-col items-center justify-center space-y-4 text-center">
-            <div className="space-y-2">
-              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                Featured Projects
-              </h2>
-            </div>
-          </div>
-          <div className="mx-auto grid max-w-5xl grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 mt-8">
-            {featuredProjects.map((project) => (
-              <ProjectCard
-                key={project.id}
-                title={project.title}
-                description={project.description}
-                image={project.image}
-                tags={project.tags}
-                link={project.link}
-              />
-            ))}
-          </div>
-          <div className="flex justify-center mt-8">
-            <Button variant="outline" asChild>
-              <Link href="/projects" className="flex items-center">
-                View All Projects
-                <ArrowRight className="ml-2 h-4 w-4" />
-              </Link>
-            </Button>
-          </div>
-        </div>
-      </section>
-
-      {/* Featured Blog Posts */}
-      <section className="w-full py-12 md:py-24 lg:py-32">
-        <div className="container px-4 md:px-6">
-          <div className="flex flex-col items-center justify-center space-y-4 text-center">
-            <div className="space-y-2">
-              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                Featured Blog Posts
-              </h2>
-            </div>
-            {featuredBlogPosts.map((blogPost) => (
-              <BlogPostCard
-                key={blogPost.id}
-                title={blogPost.title}
-                excerpt={blogPost.excerpt}
-                date={blogPost.date}
-                image={blogPost.image}
-                slug={blogPost.slug}
-              />
-            ))}
-          </div>
-          <div className="flex justify-center mt-8">
-            <Button variant="outline" asChild>
-              <Link href="/blog" className="flex items-center">
-                Read All Posts
-                <ArrowRight className="ml-2 h-4 w-4" />
-              </Link>
-            </Button>
-          </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>{" "}
+    </>
   );
 }
