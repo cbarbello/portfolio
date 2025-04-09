73705f50209a2b1c846a34111547c7f39cff8e10
Casey Barbello <caseyjbarbello@gmail.com>
2025-04-08 17:34:05 -0700

    fix broken import

--- app/projects/[slug]/projectPost.tsx
+++ app/projects/[slug]/projectPost.tsx
@@ -2,7 +2,7 @@ import { notFound } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
-import { projectPosts } from "@/app/projects/page";
+import { projectPosts } from "../project-posts";
 
 export async function generateMetadata({
   params,
